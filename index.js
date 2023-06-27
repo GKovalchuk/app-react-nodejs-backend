eb7d2abf3d0b312eeecf1e350d11307cf3a23c75
GKovalchuk <101798645+GKovalchuk@users.noreply.github.com>
2023-06-27 22:50:33 +0300

    Попытка победить Cors

--- index.js
+++ index.js
@@ -9,29 +9,40 @@ const filterData = async (pathname, dataId) => {
 	return JSON.stringify(resData);
 }
 
+const sendJson = (res, filteredData) => {
+	res.setHeader('Access-Control-Allow-Methods', "GET");
+	res.setHeader('Access-Control-Allow-Origin', "*");
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(filteredData);
+}
+
+const sendFile = (res, path) => {
+	res.setHeader('Access-Control-Allow-Methods', "GET");
+	res.setHeader('Access-Control-Allow-Origin', "*");
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	res.sendFile(path);
+}
+
 app.get('/news', (req, res) => {
-	res.sendFile(
-		`${__dirname}/data/news.json`
-	);
+	const path = `${__dirname}/data/news.json`;
+	sendFile(res, path);
 });
 app.get('/promotions', (req, res) => {
-	res.sendFile(
-		`${__dirname}/data/promotions.json`
-	);
+	const path = `${__dirname}/data/promotions.json`;
+	sendFile(res, path);
 });
 app.get('/news/:newsId', async (req, res) => {
 	const { newsId } = req.params;
 	let pathname = `${__dirname}/data/news.json`;
 	let filteredData = await filterData(pathname, newsId);
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(filteredData);
+	sendJson(res, filteredData);
 });
 app.get('/promotions/:promoId', async (req, res) => {
 	const { promoId } = req.params;
 	let pathname = `${__dirname}/data/promotions.json`;
 	let filteredData = await filterData(pathname, promoId);
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(filteredData);
+	sendJson(res, filteredData);
 });
 
 app.listen(1337, () => {
